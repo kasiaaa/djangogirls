68e73cf667b3c1cd04eb66b87355bebd5351fee9
Kasia <ka.janocha@gmail.com>
2015-05-24 15:55:07 +0000

    Nowy styl

--- SuperStronka/settings.py
+++ SuperStronka/settings.py
@@ -15,7 +15,6 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT= os.path.join(BASE_DIR, 'static/')
 STATIC_URL='/static/'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
@@ -102,3 +101,6 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
