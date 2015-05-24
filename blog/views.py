b1f363dbf3dd1ee1f3006acfdc9e70467e49ea4b
Kasia <ka.janocha@gmail.com>
2015-05-24 15:31:13 +0000

    Dodawanie postow

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+def post_new(request):
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.save()
+		return post_list(request)
+	else:
+		form = PostForm()
+	return render(request, 'blog/post_edit.html', {'form': form})
